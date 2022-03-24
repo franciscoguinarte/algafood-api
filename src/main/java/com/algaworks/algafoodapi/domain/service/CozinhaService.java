22197ca0d6d71b91902d520fe30875ea8d7e2481
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-24 17:35:20 -0300

    Adicionando camada de Service para Cozinha

---
+++ src/main/java/com/algaworks/algafoodapi/domain/service/CozinhaService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafoodapi.domain.service;
+
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import com.algaworks.algafoodapi.infraestructure.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha){
+        return cozinhaRepository.adicionar(cozinha);
+    }
+}
