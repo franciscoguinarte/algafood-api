359d4a3ce3a3c994eda716dc449bd37273fcd26a
Francisco Filho <francisco.guinarte@outlook.com.br>
2022-03-26 17:53:24 -0300

    Adicionando Controller de Restaurante

--- src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -1,4 +1,47 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.service.RestauranteService;
+import com.algaworks.algafoodapi.infraestructure.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/restaurantes")
 public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private RestauranteService restauranteService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscar(@PathVariable Long id) {
+        Restaurante restaurante = restauranteRepository.buscar(id);
+        if (id == null ||  restaurante == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(restaurante);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listar() {
+        List<Restaurante> restaurantes = restauranteRepository.listar();
+
+        if(restaurantes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(restaurantes);
+    }
+
+//    @PostMapping
+//    public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante) {
+//        restaurante = restauranteRepository.adicionar(restaurante);
+//        return ResponseEntity.ok(restaurante);
+//    }
+
+
 }
---
+++ src/main/java/com/algaworks/algafoodapi/domain/service/RestauranteService.java
@@ -0,0 +1,7 @@
+package com.algaworks.algafoodapi.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestauranteService {
+}
